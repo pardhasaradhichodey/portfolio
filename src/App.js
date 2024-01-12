493598db273ce0e74f4148688b1c0418acb49b21
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2024-01-12 12:19:00 -0800

    Projects Update

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { loadFull } from "tsparticles";
 import "./App.css";
-import About from './components/About';
-import ContactUs from './components/ContactUs';
-import Dashboard from './components/Dashboard';
-import Experience from './components/Experience';
-import NavigationBar from './components/NavigationBar';
-import Projects from './components/Projects';
-import particlesOptions from "./particles.json";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Dashboard from "./components/Dashboard";
+import Experience from "./components/Experience";
+import NavigationBar from "./components/NavigationBar";
+import Projects from "./components/Projects";
 import Sidebar from "./components/Sider";
+import particlesOptions from "./particles.json";
 
 function App() {
-    const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false);
 
-    useEffect(() => {
-        if (init) {
-            return;
-        }
+  useEffect(() => {
+    if (init) {
+      return;
+    }
 
-        initParticlesEngine(async (engine) => {
-            await loadFull(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, [init]);
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, [init]);
 
-    return (
-        <div className="App">
-            {init && <Particles options={particlesOptions}/>}
-            <Router>
-    <NavigationBar />
-      <div className='body'>
-      <Routes>
-        <Route exact path="portfolio/" element={<Dashboard />} />
-        <Route exact path="portfolio/about" element={<About />} />
-        <Route exact path="portfolio/projects" element={<Projects />} />
-        <Route exact path="portfolio/experience" element={<Experience />} />
-        <Route exact path="portfolio/contact" element={<ContactUs />} />
-      </Routes>
-      </div>
-      
-    </Router>
-    <Sidebar/>
+  return (
+    <div className="App">
+      {init && <Particles options={particlesOptions} />}
+      <Router>
+        <NavigationBar />
+        <div className="body">
+          <Routes>
+            <Route exact path="portfolio/" element={<Dashboard />} />
+            <Route exact path="portfolio/about" element={<About />} />
+            <Route exact path="portfolio/projects" element={<Projects />} />
+            <Route exact path="portfolio/experience" element={<Experience />} />
+            <Route exact path="portfolio/contact" element={<ContactUs />} />
+          </Routes>
         </div>
-    );
+      </Router>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,119 @@
 // src/components/Projects.js
 
-import React, { useState } from 'react';
-import { Container, Row, Col, Card, CardBody, CardTitle, CardText, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
-import classnames from 'classnames';
-import './Projects.css';
+import classnames from "classnames";
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  TabContent,
+  TabPane,
+} from "reactstrap";
+import "./Projects.css";
 
 const Projects = () => {
-  const [activeTab, setActiveTab] = useState('python');
+  const [activeTab, setActiveTab] = useState("Python");
 
   const toggleTab = (tab) => {
     if (activeTab !== tab) setActiveTab(tab);
   };
-
+  const practice_projects = [
+    {
+      language: "Python",
+      projects: [
+        {
+          title: "Wallet Watch",
+          Description: "",
+          link:""
+        },
+        {
+          title: "Wallet Watch",
+          Description: "",
+          link:""
+        },
+      ],
+    },
+    {
+      language: "JavaScript",
+      projects: [
+        {
+          title: "To Do List",
+          Description: "",
+          link:""
+        },
+        {
+          title: "Wallet Watch",
+          Description: "",
+          link:""
+        },
+      ],
+    },
+    {
+      language: "React",
+      projects: [
+        {
+          title: "Monoster Rolodex",
+          Description: "",
+          link:""
+        },
+        {
+          title: "JS Project",
+          Description: "",
+          link:""
+        },
+      ],
+    },
+    {
+      language: "Java",
+      projects: [
+        {
+          title: "Java Project 1",
+          Description: "",
+          link:""
+        },
+        {
+          title: "Java Project 2",
+          Description: "",
+          link:""
+        },
+      ],
+    },
+  ];
+  const projects_details = [
+    {
+      title: "Civicx",
+      Description: "",
+      link: "https://github.com/pardhasaradhichodey/Cloud_Computing_Project",
+    },
+    {
+      title: "Covid Prediction",
+      Description: "",
+      link: "https://github.com/pardhasaradhichodey/finalProjectUndergrads",
+    },
+    {
+      title: "Movie Ticket Booking Application",
+      Description: "",
+      link: "https://github.com/pardhasaradhichodey/InternshipProject.Net",
+    },
+  ];
   return (
     <Container>
       <h1>Projects</h1>
       <Row>
         {/* Main Projects Section */}
-        {[1, 2, 3].map((item) => (
+        {projects_details.map((item) => (
           <Col md={4} key={item}>
-            <Card>
+            <Card className="projectcard">
               <CardBody>
-                <CardTitle tag="h5">Project Title {item}</CardTitle>
-                <CardText>Description of project {item}.</CardText>
+                <CardTitle tag="h5"> {item.title}</CardTitle>
+                <CardText>Description of project {item.Description}.</CardText>
               </CardBody>
             </Card>
           </Col>
@@ -31,24 +122,42 @@ const Projects = () => {
 
       {/* Practice Projects Section with Tabs */}
       <Nav tabs>
-        {['python', 'javascript', 'react', 'java'].map((language) => (
-          <NavItem key={language}>
+        {practice_projects.map((language) => (
+          <NavItem key={language.language}>
             <NavLink
-              className={classnames({ active: activeTab === language })}
-              onClick={() => { toggleTab(language); }}
+              className={classnames({
+                active: activeTab === language.language,
+              })}
+              onClick={() => {
+                toggleTab(language.language);
+              }}
             >
-              {language.charAt(0).toUpperCase() + language.slice(1)}
+              {language.language}
             </NavLink>
           </NavItem>
         ))}
       </Nav>
       <TabContent activeTab={activeTab}>
-        {['python', 'javascript', 'react', 'java'].map((language) => (
-          <TabPane tabId={language} key={language}>
+        {practice_projects.map((language) => (
+          <TabPane tabId={language.language} key={language.language}>
             <Row>
               <Col sm="12">
-                <h4>{language.charAt(0).toUpperCase() + language.slice(1)} Practice Projects</h4>
+                <h4>{language.language} Practice Projects</h4>
                 {/* List of projects for each language */}
+                <Container>
+                  <Row>
+                    {language.projects.map((project) => (
+                      <Col>
+                        <Card className="projectcard">
+                          <CardBody>
+                            <CardTitle tag="h5"> {project.title}</CardTitle>
+                            <CardText>{project.Description}.</CardText>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
               </Col>
             </Row>
           </TabPane>
